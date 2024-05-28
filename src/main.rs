b8932f29565b2c0ff0cd4cd3ba4f955e079a96bc
luoni-krnl <luoni@krnl.xyz>
2024-05-28 01:23:35 -0400

    feat: added krnl_transactionRequest

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use log::info;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use std::env;
+use std::error::Error;
 use std::sync::Arc;
 
 #[derive(Serialize, Deserialize)]
@@ -11,6 +13,42 @@ pub struct RequestBody {
     jsonrpc: String,
     method: String,
     params: serde_json::Value,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TxRequest {
+    accessToken: String,
+    message: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SignatureToken {
+    signatureToken: String,
+    hash: String,
+}
+
+async fn call_token_authority(path: &str, payload: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+    let token_authority = "http://127.0.0.1:8181";
+    let url = format!("{}{}", token_authority, path);
+
+    let client = Client::new();
+    let resp = client
+        .post(&url)
+        .header("Content-Type", "application/json")
+        .body(payload.to_vec())
+        .send()
+        .await?;
+
+    if resp.status() == reqwest::StatusCode::UNAUTHORIZED {
+        return Err("transaction rejected: invalid access token".into());
+    }
+
+    if resp.status() == reqwest::StatusCode::BAD_REQUEST {
+        return Err("transaction rejected: no FaaS request specified".into());
+    }
+
+    let body = resp.bytes().await?;
+    Ok(body.to_vec())
 }
 
 #[post("/")]
@@ -26,6 +64,50 @@ async fn proxy(req_body: web::Json<Value>, client: web::Data<Arc<Client>>) -> im
             return HttpResponse::BadRequest().body(error_message);
         }
     };
+
+    println!("method {:?}", body.method);
+
+    if body.method == "krnl_transactionRequest" {
+        println!("params {:?}", body.params);
+        let tx_request: Vec<TxRequest> = match serde_json::from_value(body.params.clone()) {
+            Ok(tx_request) => tx_request,
+            Err(err) => {
+                let error_message = format!("Failed to deserialize TxRequest: {}", err);
+                info!("{}", error_message);
+                return HttpResponse::BadRequest().body(error_message);
+            }
+        };
+        println!("access_token {:?}", tx_request[0]);
+
+        let tx_request_payload = match serde_json::to_vec(&tx_request[0]) {
+            Ok(payload) => payload,
+            Err(err) => {
+                let error_message = format!("Failed to serialize TxRequest: {}", err);
+                info!("{}", error_message);
+                return HttpResponse::InternalServerError().body(error_message);
+            }
+        };
+
+        let body = match call_token_authority("/tx-request", &tx_request_payload).await {
+            Ok(body) => body,
+            Err(err) => {
+                let error_message = format!("Token Authority error: {}", err);
+                info!("{}", error_message);
+                return HttpResponse::InternalServerError().body(error_message);
+            }
+        };
+
+        let signature_token: SignatureToken = match serde_json::from_slice(&body) {
+            Ok(token) => token,
+            Err(err) => {
+                let error_message = format!("Failed to deserialize SignatureToken: {}", err);
+                info!("{}", error_message);
+                return HttpResponse::InternalServerError().body(error_message);
+            }
+        };
+
+        return HttpResponse::Ok().json(signature_token);
+    }
 
     let response = client.post(solana_url).json(&body).send().await;
 
